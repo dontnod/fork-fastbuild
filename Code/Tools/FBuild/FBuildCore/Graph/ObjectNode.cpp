fastbuild/fastbuild

Hash functions can be path agnostic if RootPath is set in Settings() function
RootPath will be automagically replaced by a special char before hashing.
This is the brute force approach but it might be enough to workaround this FastBuild limitation.

Code/Tools/FBuild/FBuildCore/Graph/ObjectNode.cpp
@@ -1035,7 +1035,7 @@ const AString & ObjectNode::GetCacheName( Job * job ) const
 
     // hash the pre-processed intput data
     ASSERT( job->GetData() );
-    uint64_t a = xxHash::Calc64( job->GetData(), job->GetDataSize() );
+    uint64_t a = FBuild::Hash64( job->GetData(), job->GetDataSize() );
 
     // hash the build "environment"
     // TODO:B Exclude preprocessor control defines (the preprocessed input has considered those already)
@@ -1046,7 +1046,7 @@ const AString & ObjectNode::GetCacheName( Job * job ) const
         const bool showIncludes = false;
         const bool finalize = false; // Don't write args to reponse file
         BuildArgs( job, args, PASS_COMPILE_PREPROCESSED, useDeoptimization, showIncludes, finalize );
-        b = xxHash::Calc32( args.GetRawArgs().Get(), args.GetRawArgs().GetLength() );
+        b = FBuild::Hash32( args.GetRawArgs().Get(), args.GetRawArgs().GetLength() );
     }
 
     // ToolChain hash
@@ -1094,7 +1094,7 @@ bool ObjectNode::RetrieveFromCache( Job * job )
             uint64_t pchKey = 0;
             if ( GetFlag( FLAG_CREATING_PCH ) && GetFlag( FLAG_MSVC ) )
             {
-                pchKey = xxHash::Calc64( cacheData, cacheDataSize );
+                pchKey = FBuild::Hash64( cacheData, cacheDataSize );
             }
 
             // do decompression
@@ -1231,7 +1231,7 @@ void ObjectNode::WriteToCache( Job * job )
                 // Dependent objects need to know the PCH key to be able to pull from the cache
                 if ( GetFlag( FLAG_CREATING_PCH ) && GetFlag( FLAG_MSVC ) )
                 {
-                    m_PCHCacheKey = xxHash::Calc64( data, dataSize );
+                    m_PCHCacheKey = FBuild::Hash64( data, dataSize );
                 }
 
                 // Output
@@ -2010,7 +2010,7 @@ bool ObjectNode::WriteTmpFile( Job * job, AString & tmpDirectory, AString & tmpF
     ASSERT( job->GetData() && job->GetDataSize() );
 
     Node * sourceFile = GetSourceFile();
-    uint32_t sourceNameHash = xxHash::Calc32( sourceFile->GetName().Get(), sourceFile->GetName().GetLength() );
+    uint32_t sourceNameHash = FBuild::Hash32( sourceFile->GetName().Get(), sourceFile->GetName().GetLength() );
 
     FileStream tmpFile;
     AStackString<> fileName( sourceFile->GetName().FindLast( NATIVE_SLASH ) + 1 );

dontnod/fork-fastbuild

Code/Tools/FBuild/FBuildCore/Graph/ObjectNode.cpp
@@ -1168,7 +1168,7 @@ const AString & ObjectNode::GetCacheName( Job * job ) const
 
     // hash the pre-processed input data
     ASSERT( m_LightCacheKey || job->GetData() );
-    const uint64_t preprocessedSourceKey = m_LightCacheKey ? m_LightCacheKey : xxHash::Calc64( job->GetData(), job->GetDataSize() );
+    const uint64_t preprocessedSourceKey = m_LightCacheKey ? m_LightCacheKey : FBuild::Hash64( job->GetData(), job->GetDataSize() );
     ASSERT( preprocessedSourceKey );
 
     // hash the build "environment"
@@ -1180,7 +1180,7 @@ const AString & ObjectNode::GetCacheName( Job * job ) const
         const bool showIncludes = false;
         const bool finalize = false; // Don't write args to reponse file
         BuildArgs( job, args, PASS_COMPILE_PREPROCESSED, useDeoptimization, showIncludes, finalize );
-        commandLineKey = xxHash::Calc32( args.GetRawArgs().Get(), args.GetRawArgs().GetLength() );
+        commandLineKey = FBuild::Hash32( args.GetRawArgs().Get(), args.GetRawArgs().GetLength() );
     }
     ASSERT( commandLineKey );
 
@@ -1232,7 +1232,7 @@ bool ObjectNode::RetrieveFromCache( Job * job )
             uint64_t pchKey = 0;
             if ( GetFlag( FLAG_CREATING_PCH ) && GetFlag( FLAG_MSVC ) )
             {
-                pchKey = xxHash::Calc64( cacheData, cacheDataSize );
+                pchKey = FBuild::Hash64( cacheData, cacheDataSize );
             }
 
             const uint32_t startDecompress = uint32_t( t.GetElapsedMS() );
@@ -1378,7 +1378,7 @@ void ObjectNode::WriteToCache( Job * job )
                 // Dependent objects need to know the PCH key to be able to pull from the cache
                 if ( GetFlag( FLAG_CREATING_PCH ) && GetFlag( FLAG_MSVC ) )
                 {
-                    m_PCHCacheKey = xxHash::Calc64( data, dataSize );
+                    m_PCHCacheKey = FBuild::Hash64( data, dataSize );
                 }
 
                 const uint32_t cachingTime = uint32_t( t.GetElapsedMS() );
@@ -2196,7 +2196,7 @@ bool ObjectNode::WriteTmpFile( Job * job, AString & tmpDirectory, AString & tmpF
     ASSERT( job->GetData() && job->GetDataSize() );
 
     Node * sourceFile = GetSourceFile();
-    uint32_t sourceNameHash = xxHash::Calc32( sourceFile->GetName().Get(), sourceFile->GetName().GetLength() );
+    uint32_t sourceNameHash = FBuild::Hash32( sourceFile->GetName().Get(), sourceFile->GetName().GetLength() );
 
     FileStream tmpFile;
     AStackString<> fileName( sourceFile->GetName().FindLast( NATIVE_SLASH ) + 1 );