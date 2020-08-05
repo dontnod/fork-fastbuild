fastbuild/fastbuild

Hash functions can be path agnostic if RootPath is set in Settings() function
RootPath will be automagically replaced by a special char before hashing.
This is the brute force approach but it might be enough to workaround this FastBuild limitation.

Code/Tools/FBuild/FBuildCore/BFF/Functions/FunctionSettings.cpp
@@ -6,6 +6,8 @@
 #include "Tools/FBuild/FBuildCore/PrecompiledHeader.h"
 
 #include "FunctionSettings.h"
+#include "Tools/FBuild/FBuildCore/FBuild.h"
+#include "Tools/FBuild/FBuildCore/FLog.h"
 #include "Tools/FBuild/FBuildCore/Graph/NodeGraph.h"
 #include "Tools/FBuild/FBuildCore/Graph/SettingsNode.h"
 
@@ -40,6 +42,18 @@ FunctionSettings::FunctionSettings()
     {
         return false;
     }
+    // "RootPath"
+    AStackString<> rootPath;
+    if ( !GetString( funcStartIter, rootPath, ".RootPath" ) )
+    {
+        return false;
+    }
+    FBuild::Get().SetRootPath( rootPath );
+    if ( !rootPath.IsEmpty() )
+    {
+        FLOG_INFO( "RootPath: '%s'", rootPath.Get() );
+    }
+
 
     return settingsNode->Initialize( nodeGraph, funcStartIter, this );
 }

dontnod/fork-fastbuild

Code/Tools/FBuild/FBuildCore/BFF/Functions/FunctionSettings.cpp
@@ -4,6 +4,8 @@
 // Includes
 //------------------------------------------------------------------------------
 #include "FunctionSettings.h"
+#include "Tools/FBuild/FBuildCore/FBuild.h"
+#include "Tools/FBuild/FBuildCore/FLog.h"
 #include "Tools/FBuild/FBuildCore/Graph/NodeGraph.h"
 #include "Tools/FBuild/FBuildCore/Graph/SettingsNode.h"
 
@@ -40,6 +42,18 @@ FunctionSettings::FunctionSettings()
     {
         return false;
     }
+    // "RootPath"
+    AStackString<> rootPath;
+    if ( !GetString( funcStartIter, rootPath, ".RootPath" ) )
+    {
+        return false;
+    }
+    FBuild::Get().SetRootPath( rootPath );
+    if ( !rootPath.IsEmpty() )
+    {
+        FLOG_OUTPUT( "RootPath: '%s'", rootPath.Get() );
+    }
+
 
     return settingsNode->Initialize( nodeGraph, funcStartIter, this );
 }